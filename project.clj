d32e03a912095405d98e8069c4cd57c2ffb2fe46
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-02-13 20:44:38 +0000

    initial project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject website "0.1.0-SNAPSHOT"
+  :description "Personal website"
+  :url "http://github.com/AndreaCrotti/website"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.439"]
+                 [com.bhauman/figwheel-main "0.1.9"]])
