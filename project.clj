446e3ec4715d2ddbbad6a77c9c9b0c33caf6a79e
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-02-17 18:21:14 +0000

    initial hugo config

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject website "0.1.0-SNAPSHOT"
-  :description "Personal website"
-  :url "http://github.com/AndreaCrotti/website"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.439"]
-                 [com.bhauman/figwheel-main "0.1.9"]
-                 [clj-http "3.9.1"]
-                 [org.clojure/data.json "0.2.6"]])
